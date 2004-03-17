003ef978a5a99134d14ee8b542f0a5e0fc12ca95
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-03-17 20:31:00 +0000

    merge with XORG-RELEASE-1 (tag XORG-CYGWIN-LAST-MERGE)

--- src/cg14_driver.c
+++ src/cg14_driver.c
@@ -20,7 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/suncg14/cg14_driver.c,v 1.7 2003/10/30 17:37:12 tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/suncg14/cg14_driver.c,v 1.6 2001/10/01 13:44:10 eich Exp $ */
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
