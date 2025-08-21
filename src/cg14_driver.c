c303ce2af91e45815205710b03f026fd1b8c303c
Alan Coopersmith <alan.coopersmith@oracle.com>
2025-08-20 17:39:09 -0700

    Strip trailing whitespace from source files
    Performed with: `git ls-files | xargs perl -i -p -e 's{[ \t]+$}{}'`
    
    `git diff -w` & `git diff -b` show no diffs from this change
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-suncg14/-/merge_requests/11>

--- src/cg14_driver.c
+++ src/cg14_driver.c
@@ -71,7 +71,7 @@ void CG14Sync(ScrnInfoPtr pScrn);
 #define CG14_MINOR_VERSION PACKAGE_VERSION_MINOR
 #define CG14_PATCHLEVEL PACKAGE_VERSION_PATCHLEVEL
 
-/* 
+/*
  * This contains the functions needed by the server after loading the driver
  * module.  It must be supplied, and gets passed back by the SetupProc
  * function in the dynamic case.  In the static case, a reference to this
@@ -234,7 +234,7 @@ CG14Probe(DriverPtr drv, int flags)
     numUsed = xf86MatchSbusInstances(CG14_NAME, SBUS_DEVICE_CG14,
 		   devSections, numDevSections,
 		   drv, &usedChips);
-				    
+
     free(devSections);
     if (numUsed <= 0)
 	return FALSE;
@@ -249,7 +249,7 @@ CG14Probe(DriverPtr drv, int flags)
 	 */
 	if(pEnt->active) {
 	    ScrnInfoPtr pScrn;
-	    
+
 	    /* Allocate a ScrnInfoRec and claim the slot */
 	    pScrn = xf86AllocateScreen(drv, 0);
 
@@ -290,7 +290,7 @@ CG14PreInit(ScrnInfoPtr pScrn, int flags)
      * not at the start of each server generation.  This means that
      * only things that are persistent across server generations can
      * be initialised here.  xf86Screens[] is (pScrn is a pointer to one
-     * of these).  Privates allocated using xf86AllocateScrnInfoPrivateIndex()  
+     * of these).  Privates allocated using xf86AllocateScrnInfoPrivateIndex()
      * are too, and should be used for data that must persist across
      * server generations.
      *
@@ -303,7 +303,7 @@ CG14PreInit(ScrnInfoPtr pScrn, int flags)
 	return FALSE;
     }
     pCg14 = GET_CG14_FROM_SCRN(pScrn);
-    
+
     /* Set pScrn->monitor */
     pScrn->monitor = pScrn->confScreen->monitor;
 
@@ -327,7 +327,7 @@ CG14PreInit(ScrnInfoPtr pScrn, int flags)
     /*********************
     deal with depth
     *********************/
-    
+
     if (!xf86SetDepthBpp(pScrn, 0, 0, 0, Support24bppFb|Support32bppFb))
 		return FALSE;
     /* Check that the returned depth is one we support */
@@ -359,12 +359,12 @@ CG14PreInit(ScrnInfoPtr pScrn, int flags)
     if (pScrn->depth > 8) {
 	rgb weight = {0, 0, 0};
 	rgb mask = {0xff, 0xff00, 0xff0000};
-                                       
+
 	if (!xf86SetWeight(pScrn, weight, mask)) {
 	    return FALSE;
 	}
     }
-                                                                           
+
     if (!xf86SetDefaultVisual(pScrn, -1))
 	return FALSE;
     else if (pScrn->depth > 8) {
@@ -375,7 +375,7 @@ CG14PreInit(ScrnInfoPtr pScrn, int flags)
 		       xf86GetVisualName(pScrn->defaultVisual));
 	    return FALSE;
 	}
-    }                                                                                                  
+    }
 
     /*
      * The new cmap code requires this to be initialised.
@@ -397,7 +397,7 @@ CG14PreInit(ScrnInfoPtr pScrn, int flags)
     /*********************
     set up clock and mode stuff
     *********************/
-    
+
     pScrn->progClock = TRUE;
 
     if(pScrn->display->virtualX || pScrn->display->virtualY) {
@@ -429,7 +429,7 @@ CG14ScreenInit(SCREEN_INIT_ARGS_DECL)
     VisualPtr visual;
     int ret;
 
-    /* 
+    /*
      * First get the ScrnInfoRec
      */
     pScrn = xf86ScreenToScrn(pScreen);
@@ -551,7 +551,7 @@ CG14SwitchMode(SWITCH_MODE_ARGS_DECL)
  * displayed location in the video memory.
  */
 /* Usually mandatory */
-static void 
+static void
 CG14AdjustFrame(ADJUST_FRAME_ARGS_DECL)
 {
     /* we don't support virtual desktops */
@@ -613,7 +613,7 @@ CG14CloseScreen(CLOSE_SCREEN_ARGS_DECL)
     xf86UnmapSbusMem(pCg14->psdp, pCg14->x32,
 		     (pCg14->psdp->width * pCg14->psdp->height));
     xf86UnmapSbusMem(pCg14->psdp, pCg14->xlut, 4096);
-    
+
     pScreen->CloseScreen = pCg14->CloseScreen;
     return (*pScreen->CloseScreen)(CLOSE_SCREEN_ARGS);
 }
@@ -687,7 +687,7 @@ CG14InitCplane24(ScrnInfoPtr pScrn)
 {
   Cg14Ptr pCg14 = GET_CG14_FROM_SCRN(pScrn);
   int size, bpp;
-              
+
   size = pScrn->virtualX * pScrn->virtualY;
   if (pScrn->bitsPerPixel > 8) {
   	bpp = 32;
@@ -697,7 +697,7 @@ CG14InitCplane24(ScrnInfoPtr pScrn)
   memset (pCg14->fb, 0, size * (bpp >> 3));
   memset (pCg14->x32, 0, size);
   memset (pCg14->xlut, 0, 0x200);
-}                                                  
+}
 
 /*
  * This initializes the card for 8 bit mode.
@@ -707,6 +707,6 @@ CG14ExitCplane24(ScrnInfoPtr pScrn)
 {
   Cg14Ptr pCg14 = GET_CG14_FROM_SCRN(pScrn);
   int bpp = 8;
-              
+
   ioctl (pCg14->psdp->fd, CG14_SET_PIXELMODE, &bpp);
 }
